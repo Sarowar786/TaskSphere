3896774d1467a043e45fd97b337b91d1fb4723bb
Sarowar786 <sarowar2287@gmail.com>
2026-01-07 13:57:59 +0600

    folder structure setup

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const { log } = require('console');
+
+const app = express();
+
+
+//middleware to handle cors
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET","PUT","POST", "DELETE"],
+        allowedHeaders:["Content-type","Authorization"],
+    })
+)
+
+//middleware
+app.use(express.json())
+
+
+//Routes
+
+// app.use('/app/auth', authRoutes);
+// app.use('/app/users', userRoutes);
+// app.use('/app/tasks', taskRoutes);
+// app.use('/app/reports', reportRoutes);
+
+//start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> log(`Server is running on port ${PORT}`))
